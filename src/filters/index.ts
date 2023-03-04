3a9f46cb0064f99ca843452e586fe3b65abc2156
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2023-03-04 00:44:13 -0600

    woopsie...

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,7 +1,9 @@
 //TODO: Implement options support, not neccesarily high priority.
+export * from "./bad-words.js";
 export * from "./reddit.js";
 export * from "./reverse.js";
 export * from "./tenor.js";
 export * from "./translate.js";
+export * from "./typo.js";
 export * from "./uwuifier.js";
-export * from "./bad-words.js";
+export * from "./zalgo.js";
