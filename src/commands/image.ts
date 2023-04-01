e69ee7beb210368486c666753058aba1330ad725
moon2324 <99777209+moon2324@users.noreply.github.com>
2023-04-01 00:05:21 -0500

    Shows what people search

--- src/commands/image.ts
+++ src/commands/image.ts
@@ -1,11 +1,26 @@
-import { Command, RegisterBehavior } from "@sapphire/framework";
-import type { ChatInputCommandInteraction } from "discord.js";
+import { RegisterBehavior } from "@sapphire/framework";
+import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import gis from "async-g-i-s";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Subcommand } from "@sapphire/plugin-subcommands";
 
-export class ImageCommand extends Command {
-  public override async chatInputRun(interaction: ChatInputCommandInteraction) {
+@ApplyOptions<Subcommand.Options>({
+  name: "image",
+  subcommands: [
+    {
+      name: "search",
+      chatInputRun: "search",
+    },
+    {
+      name: "safety",
+      chatInputRun: "safety",
+    },
+  ],
+})
+export class ImageCommand extends Subcommand {
+  public async search(interaction: ChatInputCommandInteraction) {
     try {
-      const search = interaction.options.getString("search");
+      const search = interaction.options.getString("query", true);
       if (!search)
         return interaction.reply({
           content: "Please input a search query!",
@@ -13,7 +28,7 @@ export class ImageCommand extends Command {
         });
 
       await interaction.deferReply();
-      const results = await gis(search);
+      const results = await gis(search, { safe: "on" });
       const randomResultI = Math.floor(Math.random() * results.length);
       const randomResult = results[randomResultI];
 
@@ -22,24 +37,34 @@ export class ImageCommand extends Command {
           content: `Failed to find an image for the search "${search}"!`,
         });
 
-      return interaction.editReply({ content: randomResult.url });
+      const embed = new EmbedBuilder().setImage(randomResult.url);
+
+      return interaction.editReply({
+        content: `Introducing... "${search}"!`,
+        embeds: [embed],
+      });
     } catch (error) {
       console.error(`[ERROR] (image) ${error}`);
       return interaction.editReply("Something went wrong!");
     }
   }
 
-  override registerApplicationCommands(registry: Command.Registry) {
+  override registerApplicationCommands(registry: Subcommand.Registry) {
     registry.registerChatInputCommand(
       (builder) =>
         builder
           .setName("image")
           .setDescription("Look up a random image on Google!")
-          .addStringOption((option) =>
-            option
+          .addSubcommand((builder) =>
+            builder
               .setName("search")
-              .setRequired(true)
-              .setDescription("The search query to Google!")
+              .setDescription("Search for an image using Google!")
+              .addStringOption((option) =>
+                option
+                  .setName("query")
+                  .setDescription("The image query to search for!")
+                  .setRequired(true)
+              )
           ),
       {
         behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
